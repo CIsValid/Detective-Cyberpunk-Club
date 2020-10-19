282dee99ceb73cf1ca6c1bd177bc47e9dd248b04
TheDevArray <Chrisx9800@gmail.com>
2020-10-19 15:56:40 +0200

    interaction script

--- Assets/Scripts/Player/Inspect/InspectObjectScript.cs
+++ Assets/Scripts/Player/Inspect/InspectObjectScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,10 +7,19 @@ using UnityEngine.UI;
 public class InspectObjectScript : MonoBehaviour
 {
     // Public Variables
+    
+    [Header("Player & Target")]
+    [Required]
+    public GameObject player;
+
+    private GameObject currentItemObject;
 
     [Header("Text Object")]
     [Required]
     public Text text;
+
+    [Header("Distance To Start Highlight")]
+    public float detectionDistance = 20f;
 
     [Header("Item Info")]
     public string itemName;
@@ -23,6 +33,9 @@ public class InspectObjectScript : MonoBehaviour
     private Vector3 oldPosition;
     private Quaternion oldRotation;
 
+    private CameraContoller cameraContoller;
+    private PlayerController playerController;
+
     // Camera
     private Camera mainCamera;
 
@@ -33,9 +46,14 @@ public class InspectObjectScript : MonoBehaviour
 
     private bool hasPressedToInteract;
 
-    private void Start() {
+    private void Start()
+    {
+        playerController = player.GetComponent<PlayerController>();
+        currentItemObject = this.gameObject;
+        
         m_Renderer = this.gameObject.GetComponent<MeshRenderer>();
         mainCamera = Camera.main;
+        cameraContoller = mainCamera.GetComponent<CameraContoller>();
         currentObjectPos = this.transform.localPosition;
         oldPosition = this.transform.localPosition;
         oldRotation = this.transform.localRotation;
@@ -45,7 +63,7 @@ public class InspectObjectScript : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            if(!hasPressedToInteract && m_Renderer.isVisible) text.text = interactionMessage;
+            if(!hasPressedToInteract && m_Renderer.isVisible)text.text = interactionMessage;
             else
             {
                 text.text = null;
@@ -57,6 +75,19 @@ public class InspectObjectScript : MonoBehaviour
                 StartInspection();
                 text.text = null;
             }
+
+            if (Input.GetKey(KeyCode.Escape) && hasPressedToInteract)
+            {
+                ExitInspection();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (m_Renderer.isVisible && (player.transform.position - currentObjectPos).sqrMagnitude < detectionDistance * detectionDistance)
+        {
+            HighlightItem();
         }
     }
 
@@ -69,13 +100,36 @@ public class InspectObjectScript : MonoBehaviour
         }
     }
 
+    private void HighlightItem()
+    {
+        // Highlight the item so the player knows theres an interactable there
+        Debug.Log("hi");
+    }
+
     private void StartInspection()
     {
-
+        // Blur Background except for Item being Inspected
+        // Move Object to front of camera
+        // Scale Object to camera? 
+        // Activate Inspection Controller
+        InspectionController();
+        // Deactivate Camera Controller & Player controller
+        // Play Audio file
     }
     private void ExitInspection()
     {
+        // Move object back into original position
+        // Change Scale to original Scale
+        // remove the blur effect
+        // Blur the background
+        // Deactivate Inspection Controller
+        // Activate Camera controller + Player controller
+    }
 
+    private void InspectionController()
+    {
+        // Make camera rotate around the object with mouse input
+        // Make W & S Zoom in & Zoom out
     }
 
 }
