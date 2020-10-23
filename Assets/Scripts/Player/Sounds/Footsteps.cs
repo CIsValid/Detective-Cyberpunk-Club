d18ee48e6d2d30f8a4392959ae832aa702e6e34c
TheDevArray <Chrisx9800@gmail.com>
2020-10-23 10:41:09 +0200

    Footsies
    e

---
+++ Assets/Scripts/Player/Sounds/Footsteps.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(CharacterController))]
+public class Footsteps : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    private CharacterController characterController;
+    private AudioSource audioSource;
+    private int stepCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded && characterController.velocity.magnitude > 1f &&
+            audioSource.isPlaying == false)
+        {
+            audioSource.Play();
+            stepCount++;
+        }
+        
+        switch (stepCount)
+        {
+            case 0:
+                audioSource.clip = audioClips[0];
+                break;
+            case 1:
+                audioSource.clip = audioClips[1];
+                break;
+            case 2:
+                audioSource.clip = audioClips[0];
+                stepCount = 0;
+                break;
+        }
+    }
+}
