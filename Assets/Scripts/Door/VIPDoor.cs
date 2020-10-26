3412df89aa19ac3d17f9054a049a59ee980296e4
TheDevArray <Chrisx9800@gmail.com>
2020-10-26 10:35:00 +0100

    v2

---
+++ Assets/Scripts/Door/VIPDoor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VIPDoor : MonoBehaviour
+{
+    public GameObject doorToAffect;
+    public Material newMaterial;
+
+    private GameObject target;
+    private PlayerManager playerManager;
+    private Renderer rend;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        if (!audioSource)
+        {
+            audioSource = this.gameObject.GetComponent<AudioSource>();
+        }
+        
+        rend = doorToAffect.GetComponent<Renderer>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!target)
+            {
+                target = other.gameObject;
+            }
+
+            if (!playerManager && target)
+            {
+                playerManager = target.GetComponent<PlayerManager>();
+            }
+
+            if (playerManager && target)
+            {
+                // Check if he has interacted with keycard
+
+                if (playerManager.hasKeyCard)
+                {
+                    // change material on door
+
+                    rend.material = newMaterial;
+                
+                    // Disable Collider
+                    doorToAffect.GetComponent<BoxCollider>().enabled = false;
+                    
+                    // Play sound
+                    audioSource.Play();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private GameObject keyCard;
+    private GameObject elecButton;
+
+    public bool hasPressedButton;
+    public bool hasKeyCard;
+    private Completed completedkey;
+    private Completed inspectObjectScriptbutton;
+
+    private void Start()
+    {
+        // find key with tag
+        keyCard = GameObject.FindGameObjectWithTag("keycard");
+        completedkey = keyCard.GetComponent<Completed>();
+        // find button with tag
+        //elecButton = GameObject.FindGameObjectWithTag("elecbutton");
+        //inspectObjectScriptbutton = elecButton.GetComponent<InspectObjectScript>();
+
+
+    }
+
+    private void Update()
+    {
+        // set bool to true or false depending on their interaction status here
+        if (keyCard)
+        {
+            hasKeyCard = completedkey.completed;
+        }
+        
+        /*if (elecButton)
+        {
+            hasPressedButton = inspectObjectScriptbutton.completed;
+        }*/
+        
+    }
+    
+    
+}
